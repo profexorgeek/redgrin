9692d4a84f1b605541d021263ebe01128ab7932a
Justin Johnson <justin.d.johnson@gmail.com>
2018-12-28 14:28:39 -0700

    Added network event so NetworkManager can fire custom events.

---
+++ NetworkEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedGrin
+{
+    public delegate void NetworkEvent(long entityId, object data = null);
+}
