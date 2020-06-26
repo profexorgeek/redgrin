8e6e7afb8b22b0039d2488531cf62be368b6b3bd
Justin Johnson <justin.d.johnson@gmail.com>
2020-06-25 22:30:15 -0600

    Moved and refactored INetworkArena

--- Interfaces/INetworkArena.cs
+++ Interfaces/INetworkArena.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RedGrin
+namespace RedGrin.Interfaces
 {
     /// <summary>
     /// A game arena that deals with networked objects.
@@ -18,11 +18,10 @@ namespace RedGrin
         /// After the game arena creates the visual representation of the entity, it
         /// will be stored by the NetworkManager, independent of the NetworkArena's tracking.
         /// </summary>
-        /// <param name="ownerId">The owner of the entity to create</param>
-        /// <param name="entityId">The unique ID of the entity to create</param>
+        /// <param name="uniqueId">The unique ID of the entity on the network</param>
         /// <param name="entityData">The initial state of the created entity</param>
         /// <returns>The created INetworkEntity, which will be tracked by the NetworkManager</returns>
-        INetworkEntity RequestCreateEntity(long ownerId, object entityData);
+        INetworkEntity HandleCreateEntity(ulong uniqueId, object entityData);
 
         /// <summary>
         /// Called by the network manager when a message is received to destroy an entity.
@@ -30,7 +29,7 @@ namespace RedGrin
         /// After this method is called, the NetworkManager will destroy its copy.
         /// </summary>
         /// <param name="entity">The entity to destroy.</param>
-        void RequestDestroyEntity(INetworkEntity entity);
+        void HandleDestroyEntity(INetworkEntity entity);
 
         /// <summary>
         /// Called by the network manager when a generic message is received. Generic
